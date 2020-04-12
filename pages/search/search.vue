9766316e1e94bef81b5d8d232771e6a9a5f001fb
陈月培 <1943265180@qq.com>
2020-04-12 15:20:28 +0800

    完成首页

---
+++ pages/search/search.vue
@@ -0,0 +1,38 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		//监听原生标题点击事件
+		onNavigationBarButtonTap(e) {
+			if(e.index==0){
+				uni.navigateBack({
+					delta:1
+				})
+			}
+		},
+		//监听搜索框文本变化
+		onNavigationBarSearchInputChanged(e) {
+			console.log(e.text);
+		},
+		//监听点击搜索按钮事件
+		onNavigationBarSearchInputConfirmed(e) {
+			console.log(e.text);
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
