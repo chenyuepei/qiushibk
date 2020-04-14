b3993c93fcb1de18ed8ba4e0cf28974b602886e2
陈月培 <1943265180@qq.com>
2020-04-14 21:51:55 +0800

    完成了话题详情页

--- components/index/swiper-tab-head.vue
+++ components/index/swiper-tab-head.vue
@@ -1,10 +1,11 @@
 <template>
 	<view class="uni-tab-bar">
-		<scroll-view scroll-x class="uni-swiper-tab">
+		<scroll-view scroll-x class="uni-swiper-tab" :style="scrollStyle">
 			<block v-for="(tab,index) in tabBars" :key="tab.id">
 				<view class="swiper-tab-list" 
 				:class="{'active':tabIndex==index}"
-				@tap="tabtap(index)">
+				@tap="tabtap(index)"
+				:style="scrollItemStyle">
 					{{tab.name}}
 					<view class="swiper-tab-line"></view>
 				</view>
@@ -17,7 +18,15 @@
 	export default {
 		props:{
 			tabBars:Array,
-			tabIndex:Number
+			tabIndex:Number,
+			scrollStyle:{
+				type:String,
+				default:""
+			},
+			scrollItemStyle:{
+				type:String,
+				default:""
+			}
 		},
 		methods:{
 			tabtap(index){
